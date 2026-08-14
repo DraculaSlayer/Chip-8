c62bdcb1c9773e96231a2b7daf8c1c3a2881d104
Dr4cu <johnatantiburcio@gmail.com>
2026-08-14 04:05:29 -0400

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,193 @@
+use std::fs::File;
+use std::io::prelude::*;
+use console_engine::Color;
+use console_engine::KeyCode;
+use console_engine::pixel;
+
+const SCREEN_WIDTH: usize = 64;
+const SCREEN_HEIGHT: usize = 32;
+
+const FONT: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+];
+
+struct Chip8 {
+    memory: [u8; 4096],
+    screen: [bool; SCREEN_WIDTH*SCREEN_HEIGHT],
+    pc: u16,
+    i: u16,
+    stack: [u16; 16],
+    v: [u8; 16],
+    sp: u16,
+    dl: u8,
+    sl: u8,
+}
+
+impl Chip8 {
+    fn new() -> Chip8 {
+        let mut new_chip: Chip8 = Chip8 {
+            memory: [0; 4096],
+            screen: [false; SCREEN_WIDTH*SCREEN_HEIGHT],
+            pc: 0x200,
+            i: 0,
+            stack: [0; 16],
+            v: [0; 16],
+            sp: 0,
+            dl: 0,
+            sl: 0,
+        };
+
+        new_chip.memory[0x050..0x09f+1].copy_from_slice(&FONT);
+
+        new_chip
+    }
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+    
+    fn load_archive(&mut self, path: String) {
+        let mut file = File::open(path);
+        let mut buffer = Vec::new();
+        file.expect("fallo").read_to_end(&mut buffer);
+
+        let start = 0x200 as usize;
+        let end = (0x200 as usize) + buffer.len();
+
+        self.memory[start..end].copy_from_slice(&buffer);
+    }
+    fn fetch(&mut self) -> u16 {
+        let bit_left = self.memory[self.pc as usize] as u16;
+        let bit_right = self.memory[(self.pc+1) as usize] as u16;
+
+        let op = (bit_left << 8) | bit_right;
+        self.pc += 2;
+
+        op
+    }
+
+    fn clear_screen(&mut self) {
+        self.screen = [false; SCREEN_WIDTH*SCREEN_HEIGHT];
+    }
+    fn jump(&mut self, nnn: u16) {
+        self.pc = nnn;
+    }
+    fn set(&mut self, x: u8, nnn: u16) {
+        self.push(self.pc);
+        self.v[x as usize] = nnn as u8;
+    }
+    fn add(&mut self, x: u8, nn: u8) {
+        self.v[x as usize] = self.v[x as usize].wrapping_add(nn);
+    }
+    fn set_index(&mut self, nnn: u16) {
+        self.i = nnn;
+    }
+    fn display(&mut self, vx: usize, vy: usize, n: u8) {
+
+        let x = vx as u16;
+        let y = vy as u16;
+
+        let num = n;
+        
+        self.v[0xf] = 0;
+
+        let mut flag = false;
+
+        for rows in 0..num {
+            let byte = self.memory[(self.i as usize) + (n as u16) as usize];
+
+            for row in 0..8 {
+
+                let bit = byte >> row;
+
+                if bit != 0 {
+                    let x_coord = (x + (row as u16)) as usize % SCREEN_WIDTH;
+                    let y_coord = (y + (rows as u16)) as usize / SCREEN_WIDTH;
+
+                    let coord = x_coord + SCREEN_WIDTH * y_coord;
+
+                    flag |= self.screen[coord];
+                    self.screen[coord] ^= true;
+                }
+
+            }
+
+            if flag {
+                self.v[0xf] = 1;
+            }else {
+                self.v[0xf] = 0;
+            }
+        }
+    }
+    
+    fn run(&mut self, opcode: u16) {
+        let mut x: u8 = ((opcode >> 8) & 0x000f).try_into().unwrap();
+        let mut y: u8 = ((opcode >> 4) & 0x000f).try_into().unwrap();
+        let mut n: u8 = (opcode & 0x000f).try_into().unwrap();
+        let mut nn: u8 = (opcode & 0x00ff).try_into().unwrap();
+        let mut nnn: u16 = opcode & 0x0fff;
+
+        let init = opcode & 0xf000;
+        
+        match init {
+            0x00e0 => self.clear_screen(),
+            0x1000 => self.jump(nnn),
+            0x6000 => self.set(x, nnn),
+            0x7000 => self.add(x, nn),
+            0xa000 => self.set_index(nnn),
+            0xd000 => self.display(self.v[x as usize].into(), self.v[y as usize].into(), n),
+            _ => println!("no hay instruccion"),
+        }
+    }
+}
+
+
+fn main() {
+    let mut x: Chip8 = Chip8::new();
+
+    let mut engine = console_engine::ConsoleEngine::init(64, 32, 3);
+    
+    x.load_archive("./roms/ibm.ch8".to_string());
+
+    loop {
+        engine.as_mut().expect("FALLO").wait_frame();
+        engine.as_mut().expect("FALLO").clear_screen();
+        let op = x.fetch();
+
+        x.run(op);
+        
+        for (i, pixel) in x.screen.iter().enumerate() {
+            if *pixel {
+                /*engine.as_mut().expect("FALLO").print(0, 0, format!("{} {}", *pixel, i).as_str());*/
+            
+                let x = (i % SCREEN_WIDTH) as u32;
+                let y = (i / SCREEN_WIDTH) as u32;
+
+                engine.as_mut().expect("FALLO").set_pxl(x as i32, y as i32, pixel::pxl_fg('O', Color::Green));
+            }
+        }
+
+        if engine.as_mut().expect("FALLO").is_key_pressed(KeyCode::Char('q')) {
+            break;
+        }
+
+        engine.as_mut().expect("FALLO").draw();
+    }
+
+}
